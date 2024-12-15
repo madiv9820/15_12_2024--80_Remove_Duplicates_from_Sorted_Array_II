42ce5d94c84cc6d87da9e608ceda3395a65c7c3c
madiv9820 <themathur901@gmail.com>
2024-12-15 22:02:59 +0530

    Unit Test Cases for the problem

---
+++ UnitTest.py
@@ -0,0 +1,30 @@
+from Solution import Solution
+import unittest
+from timeout_decorator import timeout
+
+class UnitTest(unittest.TestCase):
+    def setUp(self):
+        self.__testcases = {1: ([1,1,1,2,2,3], (5, [1,1,2,2,3])),
+                            2: ([0,0,1,1,1,1,2,3,3], (7, [0,0,1,1,2,3,3]))}
+        self.__obj = Solution()
+        return super().setUp()
+    
+    @timeout(0.5)
+    def test_case_1(self):
+        nums, n, output = self.__testcases[1]
+        result = self.__obj.removeDuplicates(nums)
+        self.assertIsInstance(result, int)
+        self.assertEqual(result, n)
+        nums = nums[:n]
+        self.assertTrue(all(n == o for n,o in zip(nums, output)))
+
+    @timeout(0.5)
+    def test_case_2(self):
+        nums, n, output = self.__testcases[2]
+        result = self.__obj.removeDuplicates(nums)
+        self.assertIsInstance(result, int)
+        self.assertEqual(result, n)
+        nums = nums[:n]
+        self.assertTrue(all(n == o for n,o in zip(nums, output)))
+
+if __name__ == '__main__': unittest.main()
